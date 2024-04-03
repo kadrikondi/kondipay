23030639480ac406f13ecbe8d7ef5ded956e200e
kadrikondi <abdulkadri42@gmail.com>
2024-04-03 16:19:35 +0100

    my python code

---
+++ test.py
@@ -0,0 +1,45 @@
+# name = 'idriss'
+# if name == 'idriss':
+#     print('Good morning' + name)
+# else:
+#     print('not idris')
+
+
+
+#     # Conditional Statements
+# x = 0
+# if x > 0:
+#     print("x is positive")
+# elif x == 0:
+#     print("x is zero")
+# else:
+#     print("x is negative")
+
+# for i in range(5):
+# #     print(i)
+# def greet(name):
+#     print('Good morning ' +name)
+
+# name = input('enter name')
+# greet(name)
+
+
+numbers = [1, 2, 3, 4, 5]
+fruits = ['apple', 'banana', 'orange']
+
+
+person = {'name': 'John', 'age': 30, 'city': 'New York'}
+
+
+# for n in numbers:
+#     print(n)
+print(min(max(False,-30,-4),12,7))
+
+# a= [1,2,3]
+# a = tuple(a)
+# print(a *2)
+
+# numbers[0] =5
+# print(numbers *2)
+# for n in fruits:
+#     print(n)
